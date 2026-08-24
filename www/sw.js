c96be75c9fed10b93d6f95144f46a68ac7574651
mmarcdarenz-collab <mmarcdarenz@gmail.com>
2026-08-24 21:07:02 +1000

    Add files via upload

--- www/sw.js
+++ www/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "marc-v36-debug-j2";
+const VERSION = "marc-v36-debug-j3";
 const APP_CACHE = `${VERSION}-app`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
