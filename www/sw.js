a62a7fe7c9e7819b0c4f2f5eb81839b9b6fa06ce
mmarcdarenz-collab <mmarcdarenz@gmail.com>
2026-08-24 22:58:45 +1000

    Add files via upload

--- www/sw.js
+++ www/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "marc-v36-8-phase8-training-brief-deep-regression";
+const VERSION = "marc-v36-9-phase9-completion-body-route-authoritative-brief";
 const APP_CACHE = `${VERSION}-app`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest"];
